9ef8bbbe09ec26e520b2fc83f9d2c0174530403c
Mountain Scott <mountainscott@yahoo.com>
2021-08-07 17:10:59 -0700

    added comments to clarify differences between text from bs4 objects

--- main.py
+++ main.py
@@ -8,13 +8,24 @@ URL = 'http://www.imdb.com/chart/top'
 
 def main():
     response = requests.get(URL)
+    #* response.text is like the text from right-clicking on the webpage and clicking "page source"
+    #* 'html.parser' is the default web-scraping parser for BeautifulSoup
     soup = BeautifulSoup(response.text, 'html.parser')
+    #first decide what you want to look for. in this case, year, title, actors, and rating
+    #finding the right section can be difficult.  For example the element with the titleColum class
+    # was on *line 926*, so it's best to use control+f to find the right element based on text on the orgininal page
+
+    #* select() finds all occurences of a certain tag and returns a list 
     movie_rows = soup.select('tbody tr')
+    #print(len(movie_rows))
     for movie_row in movie_rows:
+        #<class 'bs4.element.Tag'>  (each element is a class object, not a string)
+        #print(type(movie_row))
+        #* select_one() finds the first occurence of a certain tag and returns a **tag object**
         title_column = movie_row.select_one('td.titleColumn')
-        print(title_column.text)
-        rating_column = movie_row.select_one('td.ratingColumn')
-        print(rating_column.text)
+        print(title_column.attrs.get('class'))  #* same as print(title_column['class'])
+        # rating_column = movie_row.select_one('td.ratingColumn')
+        # print(rating_column.text)
         # print(movie_row.text)
         # print(movie_row.attrs)
         # print(movie_row.attrs['class'])
@@ -25,4 +36,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
